8c6e98a3c1594b454e3b0c20a049f974d28091e3
Kazuya NUMATA <numata@gmail.com>
2012-12-11 16:03:08 +0900

    Support subject selector of project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ActiveRecord::Base
-  attr_accessible :days_per_point, :name
+  attr_accessible :days_per_point, :name, :project_subjects, :subject_ids
   has_many :large_items, dependent: :destroy
   has_many :project_subjects, dependent: :destroy
+  has_many :subjects, through: :project_subjects
+  accepts_nested_attributes_for :project_subjects, allow_destroy: true
 end
