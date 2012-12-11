6518ecd31f421dbce6c75909e4822f717651de05
Kazuya NUMATA <numata@gmail.com>
2012-12-11 13:27:47 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mitsumoritaro::Application.config.secret_token = 'f57d343fa874470720cb5789472eb05d615dd73e3193ff83130b3ab3674b8f7d80c8f9adca387eadc706c6793a42f9bf61feba392d2189fa2822b2078c7afca7'
