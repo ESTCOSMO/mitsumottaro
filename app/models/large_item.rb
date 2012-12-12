d0c18dfad5bd522484580f0c1b02ed78882aafe0
Kazuya NUMATA <numata@gmail.com>
2012-12-12 22:29:08 +0900

    Change layout of dashboard

--- app/models/large_item.rb
+++ app/models/large_item.rb
@@ -3,4 +3,24 @@ class LargeItem < ActiveRecord::Base
   belongs_to :project
   has_many :medium_items, dependent: :destroy
   validates :name, presence: true
+
+  def sum_of_point_50
+    mids = medium_items.pluck(:id)
+    sids = SmallItem.where(medium_item_id: mids)
+    SubjectPoint.where(small_item_id: sids).sum(:point_50)
+  end
+
+  def sum_of_point_50_by_project_subject_id(project_subject_id)
+    mids = medium_items.pluck(:id)
+    sids = SmallItem.where(medium_item_id: mids)
+    SubjectPoint.where(small_item_id: sids, project_subject_id: project_subject_id).sum(:point_50)
+  end
+
+  def count_of_small_items
+    medium_items.map(&:count_of_small_items).inject(0) {|result, c| result + c}
+  end
+
+  def count_of_medium_items
+    medium_items.size
+  end
 end
--- app/models/medium_item.rb
+++ app/models/medium_item.rb
@@ -3,4 +3,8 @@ class MediumItem < ActiveRecord::Base
   belongs_to :large_item
   has_many :small_items, dependent: :destroy
   validates :name, presence: true
+
+  def count_of_small_items
+    small_items.size
+  end
 end
