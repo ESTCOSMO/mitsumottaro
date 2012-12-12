8a1c33a38d853f4564cdec566149c58846acd6b2
Kazuya NUMATA <numata@gmail.com>
2012-12-12 15:45:07 +0900

    Implement subject_points#destroy

--- app/controllers/subject_points_controller.rb
+++ app/controllers/subject_points_controller.rb
@@ -1,14 +1,28 @@
 class SubjectPointsController < ApplicationController
+  before_filter :set_small_item_to_variable
+  before_filter :set_subject_point_to_variable
+
   def create
     ProjectSubject.transaction do
-      small_item = Project.find(params[:project_id]).large_items.find(params[:large_item_id]).medium_items.find(params[:medium_item_id]).small_items.find(params[:small_item_id])
-      subject_point = small_item.subject_points.find_by_project_subject_id(params[:subject_point][:project_subject_id])
-
-      subject_point && subject_point.destroy
-      created_subject_point = small_item.subject_points.create!(params[:subject_point])
+      @subject_point && @subject_point.destroy
+      created_subject_point = @small_item.subject_points.create!(params[:subject_point])
       render nothing: true, status: :ok
     end
   rescue ActiveRecord::RecordInvalid => e
     render json: e.record.errors.full_messages, status: :bad_request
+  end
+
+  def destroy
+    @subject_point && @subject_point.destroy
+    render nothing: true, status: :ok
+  end
+
+  private
+  def set_small_item_to_variable
+    @small_item = Project.find(params[:project_id]).large_items.find(params[:large_item_id]).medium_items.find(params[:medium_item_id]).small_items.find(params[:small_item_id])
+  end
+
+  def set_subject_point_to_variable
+    @subject_point = @small_item.subject_points.find_by_project_subject_id(params[:subject_point][:project_subject_id])
   end
 end
