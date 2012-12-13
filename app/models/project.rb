56589316f689fd5fcc448e3a269917e82956d376
Kazuya NUMATA <numata@gmail.com>
2012-12-13 11:56:46 +0900

    Fix ordering for the relation between project_subjects and subjects
    It should be more durable way.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :days_per_point, :name, :project_subjects, :subject_ids
   has_many :large_items, dependent: :destroy, order: :position
   has_many :project_subjects, dependent: :destroy, order: :position
-  has_many :subjects, through: :project_subjects, order: :position
+  has_many :subjects, through: :project_subjects, order: "project_subjects.position"
   accepts_nested_attributes_for :project_subjects, allow_destroy: true
   validates :name, presence: true
 end
