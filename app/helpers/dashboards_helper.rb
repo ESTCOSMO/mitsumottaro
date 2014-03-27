8080b2d3c9f8b15ed9465b706acf429731217951
irasally <irasally@gmail.com>
2014-03-27 12:10:08 +0900

    Added helper to draw arrow-link.

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,9 @@
+# -*- coding: utf-8 -*-
 module DashboardsHelper
+  def up_arrow_link_to_unless(condition, options={})
+    link_to_unless(condition, '↑', options, class: 'arrow'){ }
+  end
+  def down_arrow_link_to_unless(condition, options={})
+    link_to_unless(condition, '↓', options, class: 'arrow'){ }
+  end
 end
