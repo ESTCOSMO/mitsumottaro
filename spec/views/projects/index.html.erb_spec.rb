a2acacd029ee16f584a4fb9985a5b79d06dcd5eb
irasally <irasally@gmail.com>
2014-04-04 18:48:49 +0900

    Changed spec from assert_select to rendered matching.

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -21,6 +21,7 @@ describe "projects/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "2014/03/03 10:00:00".to_s, :count => 2
+    rendered.should match(/2014\/03\/03 10:00:00/)
+    #assert_select "tr>td", :text => "2014/03/03 10:00:00".to_s, :count => 2
   end
 end
