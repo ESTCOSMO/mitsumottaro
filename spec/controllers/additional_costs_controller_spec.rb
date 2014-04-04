009cbc1db150fda7ca924170d0aaa84208b928c0
irasally <irasally@gmail.com>
2014-04-04 17:30:11 +0900

    Added url params.

--- spec/controllers/additional_costs_controller_spec.rb
+++ spec/controllers/additional_costs_controller_spec.rb
@@ -2,25 +2,31 @@ require 'spec_helper'
 
 describe AdditionalCostsController do
 
+  before do
+    @project = Project.new(name: "Sample", days_per_point: 0.5)
+    @project.save!
+  end
+
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
+    before{ get 'index', { project_id: @project.id } }
+    subject{ response }
+    it{ should be_success }
   end
 
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
+    before{ get 'new', { project_id: @project.id } }
+    subject{ response }
+    it{ should be_success }
   end
 
   describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
+    before do
+      @additional_cost = @project.additional_costs.build(name: 'Additional', price: 10000)
+      @additional_cost.save!
+      get 'edit', { project_id: @project.id , id: @additional_cost.id }
     end
+    subject{ response }
+    it{ should be_success }
   end
 
 end
