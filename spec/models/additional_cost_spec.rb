e3881bf202e4b88d69dd9e67982a3ab7e329458f
irasally <irasally@gmail.com>
2014-04-07 15:07:47 +0900

    Added additional_cost spec.

--- spec/models/additional_cost_spec.rb
+++ spec/models/additional_cost_spec.rb
@@ -1,5 +1,29 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe AdditionalCost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @addtional_cost = AdditionalCost.new(project_id: 1, name: '追加コスト', price: 100000, position: 1) }
+  subject { @addtional_cost }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:price) }
+  it { should respond_to(:position) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @addtional_cost.name = " " }
+    it{ should_not be_valid }
+  end
+
+  describe "when price is not present" do
+    before { @addtional_cost.price = " " }
+    it{ should_not be_valid }
+  end
+
+  describe "when position is not present" do
+    before { @addtional_cost.position = " " }
+    it{ should be_valid }
+  end
+
 end
