9e70d0ca8b05d471218a09895a0163bf36a21123
irasally <irasally@gmail.com>
2014-04-07 15:07:21 +0900

    Added template_task spec.

--- spec/models/template_task_spec.rb
+++ spec/models/template_task_spec.rb
@@ -1,5 +1,28 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe TemplateTask do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @template_task = TemplateTask.new(name: "要件定義", price_per_day: 50000, position: 3) }
+  subject { @template_task }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:position) }
+  it { should respond_to(:price_per_day) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @template_task.name = " " }
+    it{ should_not be_valid }
+  end
+
+  describe "when price_per_day is not present" do
+    before { @template_task.price_per_day = "" }
+    it{ should_not be_valid }
+  end
+
+  describe "when position is not present" do
+    before { @template_task.position = "" }
+    it{ should be_valid }
+  end
 end
