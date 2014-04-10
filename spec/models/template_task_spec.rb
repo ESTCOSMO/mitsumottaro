b399261853b296f2bac1f28c6cc70572777c80b5
irasally <irasally@gmail.com>
2014-04-10 11:44:48 +0900

    Add a new describe for validation spec

--- spec/models/template_task_spec.rb
+++ spec/models/template_task_spec.rb
@@ -2,27 +2,31 @@
 require 'spec_helper'
 
 describe TemplateTask do
-  before { @template_task = TemplateTask.new(name: "要件定義", price_per_day: 50000, position: 3) }
+  before { @template_task = TemplateTask.new }
   subject { @template_task }
 
   it { should respond_to(:name) }
   it { should respond_to(:position) }
   it { should respond_to(:price_per_day) }
 
-  it { should be_valid }
+  describe "template_task validation" do
+    before { @template_task = TemplateTask.new(name: "要件定義", price_per_day: 50000, position: 3) }
+    subject { @template_task }
+    it { should be_valid }
 
-  describe "when name is not present" do
-    before { @template_task.name = " " }
-    it{ should_not be_valid }
-  end
+    context "when name is not present" do
+      before { @template_task.name = " " }
+      it{ should_not be_valid }
+    end
 
-  describe "when price_per_day is not present" do
-    before { @template_task.price_per_day = "" }
-    it{ should_not be_valid }
-  end
+    context "when price_per_day is not present" do
+      before { @template_task.price_per_day = "" }
+      it{ should_not be_valid }
+    end
 
-  describe "when position is not present" do
-    before { @template_task.position = "" }
-    it{ should be_valid }
+    context "when position is not present" do
+      before { @template_task.position = "" }
+      it{ should be_valid }
+    end
   end
 end
