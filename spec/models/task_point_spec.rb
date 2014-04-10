427949b3fc02297b0cacce1be2a1a61155dd8be5
irasally <irasally@gmail.com>
2014-04-10 11:42:48 +0900

    Add a new describe for validation spec

--- spec/models/task_point_spec.rb
+++ spec/models/task_point_spec.rb
@@ -1,38 +1,42 @@
 require 'spec_helper'
 
 describe TaskPoint do
-  before { @task_point = TaskPoint.new(story_id: 1, project_task_id: 1, point_50: 20, point_90: 50) }
+  before { @task_point = TaskPoint.new }
   subject { @task_point }
 
   it { should respond_to(:project_task_id) }
   it { should respond_to(:point_50) }
   it { should respond_to(:point_90) }
 
-  it { should be_valid }
+  describe "task_point validation" do
+    before { @task_point = TaskPoint.new(story_id: 1, project_task_id: 1, point_50: 20, point_90: 50) }
+    subject { @task_point }
+    it { should be_valid }
 
-  describe "when project_task_id is not present" do
-    before { @task_point.project_task_id = "" }
-    it{ should_not be_valid }
-  end
+    context "when project_task_id is not present" do
+      before { @task_point.project_task_id = "" }
+      it{ should_not be_valid }
+    end
 
-  describe "when point_50 is not numeric" do
-    before { @task_point.point_50 = "abc" }
-    it{ should_not be_valid }
-  end
+    context "when point_50 is not numeric" do
+      before { @task_point.point_50 = "abc" }
+      it{ should_not be_valid }
+    end
 
-  describe "when point_50 is not integer" do
-    before { @task_point.point_50 = 14.5 }
-    it{ should_not be_valid }
-  end
+    context "when point_50 is not integer" do
+      before { @task_point.point_50 = 14.5 }
+      it{ should_not be_valid }
+    end
 
-  describe "when point_90 is not numeric" do
-    before { @task_point.point_90 = "abc" }
-    it{ should_not be_valid }
-  end
+    context "when point_90 is not numeric" do
+      before { @task_point.point_90 = "abc" }
+      it{ should_not be_valid }
+    end
 
-  describe "when point_90 is not integer" do
-    before { @task_point.point_90 = 8.5 }
-    it{ should_not be_valid }
+    context "when point_90 is not integer" do
+      before { @task_point.point_90 = 8.5 }
+      it{ should_not be_valid }
+    end
   end
 
   describe "square_of_diff method" do
