c95f1c1667145c3bc991ccd20857e71a6ef8aa6f
irasally <irasally@gmail.com>
2014-04-11 14:10:08 +0900

    Add category model's spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,161 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "field definitions" do
+    subject { Category.new }
+    it { should respond_to(:name) }
+    it { should respond_to(:position) }
+    it { should respond_to(:remarks) }
+    it { should respond_to(:sub_categories) }
+  end
+
+  describe "validation" do
+    context "when input valid data," do
+      subject { Category.new(project_id: 1, name: "Category", position: 1, remarks: "備考") }
+      it { should be_valid }
+    end
+    context "when name is not present," do
+      subject { Category.new(name: "") }
+      it{ should have(1).error_on(:name) }
+    end
+    context "when position is not present," do
+      subject { Category.new(position: " ") }
+      it{ should_not have(1).error_on(:position) }
+    end
+    context "when remarks is not present," do
+      subject { Category.new(remarks: " ") }
+      it{ should_not have(1).error_on(:remarks) }
+    end
+  end
+
+  context "when category destroy," do
+    before do
+      @category = Category.new(project_id: 1, name: "Category")
+      @category.sub_categories.build(name: "SubCategory")
+      @category.save!
+    end
+    subject{ @category }
+    it "categoryを削除したらstoryも削除されること" do
+      SubCategory.where(category_id: @category.id).size.should eq 1
+      @category.destroy!
+      SubCategory.where(category_id: @category.id).size.should eq 0
+    end
+  end
+
+  describe "count_of_sub_categories" do
+    context "when having no sub_categories," do
+      before { @category = Category.new(project_id: 1, name: "Category") }
+      subject{ @category.count_of_sub_categories }
+      it{ should eq 0 }
+    end
+    context "when having some sub_categories," do
+      before do
+        @category = Category.new(project_id: 1, name: "Category")
+        @category.sub_categories.build(name: "SubCategory1")
+        @category.sub_categories.build(name: "SubCategory2")
+      end
+      subject{ @category.count_of_sub_categories }
+      it{ should eq 2 }
+    end
+  end
+
+  describe "count_of_sub_stories" do
+    let(:sub_category1) do
+      sub_category = SubCategory.new(name: "SubCategory1")
+      sub_category.stories.build(name: "Story1")
+      sub_category.stories.build(name: "Story2")
+      sub_category
+    end
+    let(:sub_category2) do
+      sub_category = SubCategory.new(name: "SubCategory2")
+      sub_category.stories.build(name: "Story1")
+      sub_category.stories.build(name: "Story2")
+      sub_category.stories.build(name: "Story3")
+      sub_category
+    end
+    context "when having no stories," do
+      before { @category = Category.new(project_id: 1, name: "Category") }
+      subject{ @category.count_of_stories }
+      it{ should eq 0 }
+    end
+    context "when having some stories," do
+      before do
+        @category = Category.new(project_id: 1, name: "Category")
+        @category.sub_categories << sub_category1
+        @category.sub_categories << sub_category2
+      end
+      subject{ @category.count_of_stories }
+      it{ should eq 5 }
+    end
+  end
+
+  describe "sum_of_point" do
+    let(:project_task1){ ProjectTask.create(name:"要件定義", price_per_day: 50000) }
+    let(:project_task2){ ProjectTask.create(name:"設計", price_per_day: 40000) }
+    let(:project_task3){ ProjectTask.create(name:"試験", price_per_day: 45000) }
+    let(:project_task4){ ProjectTask.create(name:"マニュアル", price_per_day: 30000) }
+    let(:project_task5){ ProjectTask.create(name:"導入", price_per_day: 55000) }
+    let(:story1) do
+      story = Story.new(name: "Story1")
+      story.task_points.build(project_task_id: project_task1.id, point_50: 3, point_90: 5)
+      story.task_points.build(project_task_id: project_task1.id, point_50: 5, point_90: 8)
+      story.task_points.build(project_task_id: project_task2.id, point_50: 8, point_90: 13)
+      story.task_points.build(project_task_id: project_task3.id, point_50: 2, point_90: 5)
+      story
+    end
+    let(:story2) do
+      story = Story.new(name: "Story2")
+      story.task_points.build(project_task_id: project_task1.id, point_50: 1, point_90: 2)
+      story.task_points.build(project_task_id: project_task3.id, point_50: 2, point_90: 3)
+      story.task_points.build(project_task_id: project_task4.id, point_50: 8, point_90: 13)
+      story.task_points.build(project_task_id: project_task5.id, point_50: 5, point_90: 8)
+      story
+    end
+    let(:story3) do
+      story = Story.new(name: "Story3")
+      story.task_points.build(project_task_id: project_task3.id, point_50: 3, point_90: 5)
+      story.task_points.build(project_task_id: project_task3.id, point_50: 5, point_90: 8)
+      story.task_points.build(project_task_id: project_task4.id, point_50: 1, point_90: 3)
+      story.task_points.build(project_task_id: project_task5.id, point_50: 2, point_90: 5)
+      story
+    end
+    let(:sub_category1) do
+      sub_category = SubCategory.new(name: "SubCategory1")
+      sub_category.stories << story1
+      sub_category.stories << story2
+      sub_category
+    end
+    let(:sub_category2) do
+      sub_category = SubCategory.new(name: "SubCategory2")
+      sub_category.stories << story3
+      sub_category
+    end
+    before do
+      @category = Category.new(project_id: 1, name: "Category")
+      @category.sub_categories << sub_category1
+      @category.sub_categories << sub_category2
+      @category.save!
+    end
+    describe "sum_of_point_50 method" do
+      subject{ @category.sum_of_point_50 }
+      it{ should eq 45 }
+    end
+    describe "sum_of_point_50_by_project_task_id method" do
+      subject{ @category.sum_of_point_50_by_project_task_id(3) }
+      it{ should eq 12 }
+    end
+    describe "sum_of_square_of_diff method" do
+      subject{ @category.sum_of_square_of_diff }
+      it{ should eq 109 }
+    end
+    describe "total_price method" do
+      subject{ @category.total_price(1.2, 0.5) }
+      it{ should eq 1179000 }
+    end
+  end
+
+  describe "dup_deep" do
+    pending "項目複製のテストを実装すること"
+  end
 end
