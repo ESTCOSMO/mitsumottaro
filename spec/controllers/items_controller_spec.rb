ab343b8fab91aca1b4af026196f8123a66ddebca
irasally <irasally@gmail.com>
2014-04-25 18:03:13 +0900

    Add to item-copying controller-action specs

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe ItemsController do
@@ -366,6 +367,98 @@ describe ItemsController do
           @stories[0].should eq @story1
           @stories[1].should eq @story3
           @stories[2].should eq @story2
+        end
+      end
+    end
+  end
+  describe "POST 'copy':" do
+    before do
+      @project_task = @project.project_tasks.create(name: "ProjectTask", price_per_day: 40000)
+      @category1 = @project.categories.build(name: "Category1")
+      @category2 = @project.categories.build(name: "Category2")
+      @sub_category1 = @category1.sub_categories.build(name: "SubCategory1")
+      @sub_category2 = @category2.sub_categories.build(name: "SubCategory2")
+      @story1 = @sub_category1.stories.build(name: "Story1")
+      @story2 = @sub_category2.stories.build(name: "Story2")
+      @task_point1 = @story1.task_points.build(point_50: 1, point_90: 3, project_task_id: @project_task.id)
+      @task_point2 = @story2.task_points.build(point_50: 5, point_90: 8, project_task_id: @project_task.id)
+      @project.save!
+    end
+    context "case to copy category, " do
+      before do
+        xhr :post, :copy, { project_id: @project.id, id: @category1.id }
+      end
+      describe "check response status" do
+        subject{ response.status }
+        it{ should eq  Rack::Utils::SYMBOL_TO_STATUS_CODE[:ok] }
+      end
+      describe "check the data is copied" do
+        subject{ Project.find(@project.id).categories.size }
+        it { should eq 3 }
+      end
+      describe "check the copied data is correct" do
+        before{ @copied_category = Category.where(project_id: @project.id).order(:id).last }
+        specify do
+          @copied_category.name = @category1.name + "（コピー）"
+          @copied_category.sub_categories.each do |sub_category|
+            sub_category.name = @sub_category1.name
+            sub_category.stories.each do |story|
+              story.name = @story1.name
+              story.task_points.each do |points|
+                points.point_50 = @task_point1.point_50
+                points.point_90 = @task_point1.point_90
+              end
+            end
+          end
+        end
+      end
+    end
+    context "case to copy sub_category, " do
+      before do
+        xhr :post, :copy, { project_id: @project.id, category_id: @category1.id, id: @sub_category1.id, dst_category_id: @category2.id }
+      end
+      describe "check response status" do
+        subject{ response.status }
+        it{ should eq  Rack::Utils::SYMBOL_TO_STATUS_CODE[:ok] }
+      end
+      describe "check the data is copied" do
+        subject{ Category.find(@category2.id).sub_categories.size }
+        it { should eq 2 }
+      end
+      describe "check the copied data is correct" do
+        before{ @copied_sub_category = SubCategory.where(category_id: @category2.id).order(:id).last }
+        specify do
+          @copied_sub_category.name = @sub_category1.name + "（コピー）"
+          @copied_sub_category.stories.each do |story|
+            story.name = @story1.name
+            story.task_points.each do |points|
+              points.point_50 = @task_point1.point_50
+              points.point_90 = @task_point1.point_90
+            end
+          end
+        end
+      end
+    end
+    context "case to copy story, " do
+      before do
+        xhr :post, :copy, { project_id: @project.id, category_id: @category1.id, sub_category_id: @sub_category1.id, id: @story1.id, dst_category_id: @category2.id, dst_sub_category_id: @sub_category2.id }
+      end
+      describe "check response status" do
+        subject{ response.status }
+        it{ should eq  Rack::Utils::SYMBOL_TO_STATUS_CODE[:ok] }
+      end
+      describe "check the data is copied" do
+        subject{ SubCategory.find(@sub_category2.id).stories.size }
+        it { should eq 2 }
+      end
+      describe "check the copied data is correct" do
+        before{ @copied_story = Story.where(sub_category_id: @sub_category2.id).order(:id).last }
+        specify do
+          @copied_story.name = @story1.name + "（コピー）"
+          @copied_story.task_points.each do |points|
+            points.point_50 = @task_point1.point_50
+            points.point_90 = @task_point1.point_90
+          end
         end
       end
     end
