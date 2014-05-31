22cfbea5036c6a0a2f4980a134198a91ae2e017b
Kazuya Numata <numata@gmail.com>
2014-05-31 23:53:23 +0900

    Remove unnecessory methods from items_controller
    These methods are implemented in api now

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,58 +2,6 @@
 class ItemsController < ApplicationController
   before_filter :set_project_to_variable
 
-  def create
-    category_id = params[:category_id]
-    sub_category_id = params[:sub_category_id]
-
-    if category_id.blank?
-      @project.categories.create!(permitted_params_of_category)
-    elsif sub_category_id.blank?
-      @project.categories.find(category_id).sub_categories.create!(permitted_params_of_sub_category)
-    else
-      @project.categories.find(category_id).sub_categories.find(sub_category_id).stories.create!(permitted_params_of_story)
-    end
-    render nothing: true, status: :ok
-  rescue ActiveRecord::RecordInvalid => e
-    render json: e.record.errors.full_messages, status: :bad_request
-  end
-
-  def update
-    item = find_item_from_params
-
-    category_id = params[:category_id]
-    sub_category_id = params[:sub_category_id]
-    if category_id.blank?
-      item.update_attributes!(permitted_params_of_category)
-    elsif sub_category_id.blank?
-      item.update_attributes!(permitted_params_of_sub_category)
-    else
-      item.update_attributes!(permitted_params_of_story)
-    end
-    render nothing: true, status: :ok
-  rescue ActiveRecord::RecordInvalid => e
-    render json: e.record.errors.full_messages, status: :bad_request
-  end
-
-  def destroy
-    find_item_from_params.destroy
-    render nothing: true, status: :ok
-  end
-
-  def move_higher
-    item = find_item_from_params
-    item.move_higher
-
-    redirect_to project_dashboard_path(@project, anchor: make_anchor_from_params)
-  end
-
-  def move_lower
-    item = find_item_from_params
-    item.move_lower
-
-    redirect_to project_dashboard_path(@project, anchor: make_anchor_from_params)
-  end
-
   def copy
     item = find_item_from_params
     copy_item = item.dup_deep(@project.org_project_task_id_map)
@@ -97,29 +45,5 @@ class ItemsController < ApplicationController
     else
       @project.categories.find(category_id).sub_categories.find(sub_category_id).stories.find(params[:id])
     end
-  end
-
-  def make_anchor_from_params
-    category_id = params[:category_id]
-    sub_category_id = params[:sub_category_id]
-    if category_id.blank?
-      view_context.make_anchor(params[:id])
-    elsif sub_category_id.blank?
-      view_context.make_anchor(category_id, params[:id])
-    else
-      view_context.make_anchor(category_id, sub_category_id, params[:id])
-    end
-  end
-
-  def permitted_params_of_category
-    params.require(:category).permit(:name, :remarks)
-  end
-
-  def permitted_params_of_sub_category
-    params.require(:sub_category).permit(:name, :remarks)
-  end
-
-  def permitted_params_of_story
-    params.require(:story).permit(:name, :remarks)
   end
 end
