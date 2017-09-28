55c019a19744986b01a5dfa603c0461c8734fba3
Kazuya Numata <numata@antas.jp>
2017-09-28 12:58:50 +0900

    Shut rubocop Style/SymbolArray up

--- config/routes.rb
+++ config/routes.rb
@@ -27,54 +27,54 @@ Mitsumoritaro::Application.routes.draw do
         get 'convert' => 'dashboards#convert', as: :convert
       end
     end
-    resources :project_tasks, except: [:show, :new, :edit] do
+    resources :project_tasks, except: %i[show new edit] do
       member do
         get 'move_higher' => 'project_tasks#move_higher', as: :move_higher
         get 'move_lower' => 'project_tasks#move_lower', as: :move_lower
       end
     end
-    resources :categories, only: [:create, :update, :destroy], controller: :items do
+    resources :categories, only: %i[create update destroy], controller: :items do
       member do
         get 'move_higher' => 'items#move_higher', as: :move_higher
         get 'move_lower' => 'items#move_lower', as: :move_lower
         post 'copy' => 'items#copy', as: :copy
       end
-      resources :sub_categories, only: [:create, :update, :destroy] , controller: :items do
+      resources :sub_categories, only: %i[create update destroy] , controller: :items do
         member do
           get 'move_higher' => 'items#move_higher', as: :move_higher
           get 'move_lower' =>  'items#move_lower', as: :move_lower
           post 'copy' => 'items#copy', as: :copy
         end
-        resources :stories, only: [:create, :update, :destroy] , controller: :items do
+        resources :stories, only: %i[create update destroy] , controller: :items do
           member do
             get 'move_higher' => 'items#move_higher', as: :move_higher
             get 'move_lower' => 'items#move_lower', as: :move_lower
             post 'copy' => 'items#copy', as: :copy
           end
-          resources :task_points, only: [:create, :destroy]
+          resources :task_points, only: %i[create destroy]
         end
       end
     end
   end
 
   namespace :api do
-    resources :projects, only: [:show, :create, :update] do
-      resources :categories, except: [:index, :new, :edit] do
+    resources :projects, only: %i[show create update] do
+      resources :categories, except: %i[index new edit] do
         member do
           patch 'move_higher' => 'categories#move_higher', as: :move_higher
           patch 'move_lower' => 'categories#move_lower', as: :move_lower
         end
-        resources :sub_categories, except: [:index, :new, :edit] do
+        resources :sub_categories, except: %i[index new edit] do
           member do
             patch 'move_higher' => 'sub_categories#move_higher', as: :move_higher
             patch 'move_lower' => 'sub_categories#move_lower', as: :move_lower
           end
-          resources :stories, except: [:index, :new, :edit] do
+          resources :stories, except: %i[index new edit] do
             member do
               patch 'move_higher' => 'stories#move_higher', as: :move_higher
               patch 'move_lower' => 'stories#move_lower', as: :move_lower
             end
-            resources :task_points, only: [:create, :destroy]
+            resources :task_points, only: %i[create destroy]
           end
         end
       end
